8b2f1e4661ef20ac219bde607c4f05fd17e3f32c
Gustavo Charamba <gucharamba@gmail.com>
2024-07-19 17:47:45 -0300

    global colors and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import PageRouter from "./routes/routes";
+import GlobalStyle from "./global/globalStyle";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <PageRouter/>
     </BrowserRouter>
   );
---
+++ frontend/src/global/globalColors.js
@@ -0,0 +1,4 @@
+export const colors = {
+    purple: "#8A05BE",
+    lightGray: "#F5F5F5"
+}
